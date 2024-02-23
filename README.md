6027fefd15c82742bcd7076970fd8acd1443b4d0
Rishi <rishimaheshwari040@gmail.com>
2024-02-23 16:47:29 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# Frontend Developer Assignment: Star Wars Planets Directory
+
+## Getting Started
+
+### Run Locally
+
+\
+Install Dependencies
+
+```bash
+  npm install
+```
+
+Start the Client
+
+```bash
+  npm run dev
+```
---
+++ src/component/Banner.jsx
@@ -0,0 +1,14 @@
+import "../Style/Banner.css";
+import Zoom from "react-reveal/Zoom";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <Zoom>
+        <h1>Star Wars Planets Directory</h1>
+      </Zoom>
+    </div>
+  );
+};
+
+export default Banner;
